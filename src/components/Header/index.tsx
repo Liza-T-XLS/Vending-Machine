63a2a0c64f6c834d458d117cf2d624abb8cb834f
Liza-T-XLS <shapi94@gmail.com>
2021-09-08 21:36:17 +0200

    Styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ import './header.scss';
 
 const Header = () => (
   <header className="header">
-    <h1>Vending Machine</h1>
+    <h1><span className="signSpan">Vending-Ma</span>chine</h1>
   </header>
 );
 
--- src/components/VM/index.tsx
+++ src/components/VM/index.tsx
@@ -4,12 +4,46 @@ import React from 'react';
 
 import './vm.scss';
 
+import soda from '../../images/soda.svg';
+
 // == Component
 
 const VM = () => (
   <main>
     <div className="vm">
-      <p>vending machine</p>
+      <div className="left">
+        <div className="sign">Vending-Machine</div>
+        <div className="products">
+          <div className="product"><img className="productImg" src={soda} alt="soda" /></div>
+          <div className="product">Product 002</div>
+          <div className="product">Product 003</div>
+          <div className="product">Product 004</div>
+          <div className="product">Product 005</div>
+          <div className="product">Product 006</div>
+          <div className="product">Product 007</div>
+          <div className="product">Product 008</div>
+          <div className="product">Product 009</div>
+        </div>
+        <div className="pickupBox">Pick-up Box</div>
+      </div>
+      <div className="right">
+        <div className="screen">
+          <p className="instructions">Please select your product</p>
+          <div className="pad">
+            <div className="padNumber">001</div>
+            <div className="padNumber">002</div>
+            <div className="padNumber">003</div>
+            <div className="padNumber">004</div>
+            <div className="padNumber">005</div>
+            <div className="padNumber">006</div>
+            <div className="padNumber">007</div>
+            <div className="padNumber">008</div>
+            <div className="padNumber">009</div>
+          </div>
+        </div>
+        <div className="cardReader">Card Reader</div>
+        <div className="refillButton">Refill Button</div>
+      </div>
     </div>
   </main>
 );
