7272e6cb574333ad7d907b68d7ffcb84f5c460d5
Liza-T-XLS <shapi94@gmail.com>
2021-09-14 19:09:57 +0200

    Styling: responsive design for tablet and desktop

--- src/components/VM/Product/index.tsx
+++ src/components/VM/Product/index.tsx
@@ -14,17 +14,13 @@ type Props = {
 };
 
 const Product = ({ item, selectedProduct, setSelectedProduct }: Props) => {
-  // useEffect(() => {
-  //   console.log(selectedProduct);
-  // }, [selectedProduct]);
-
   const itemVMId = item.id.toString().length === 1 ? `00${item.id}` : item.id;
   
-  const productImgClassName = classNames('productImg', {selected: selectedProduct === item.id && window.matchMedia('(max-width: 460px)').matches})
+  const productImgClassName = classNames('productImg', {selected: selectedProduct === item.id && window.matchMedia('(max-width: 767px)').matches})
 
   const onSelectHandler = (event: MouseEvent<HTMLDivElement>): void => {
     const target = event.currentTarget as HTMLDivElement;
-    if(window.matchMedia('(max-width: 460px)').matches) {
+    if(window.matchMedia('(max-width: 767px)').matches) {
       setSelectedProduct(parseInt(target.id));
     }
   };
